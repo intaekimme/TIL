47060ccb79eb078ff86c3550e061b7077c1cff21
InTae <kit938639@gmail.com>
2023-01-17 00:23:09 +0900

    기준을 새로 설정하여 완전탐색 - 독서실의 거리두기 5_refactoring

--- algo/codetree/java/complete_search_by_setting_a_new_standard/Main_1.java
+++ algo/codetree/java/complete_search_by_setting_a_new_standard/Main_1.java
@@ -8,40 +8,47 @@ import java.io.*;
 public class Main_1 {
 
     static int n;
-    static char[] origin, copy;
+    static char[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
-        origin = br.readLine().toCharArray();
+        arr = br.readLine().toCharArray();
 
         int max = Integer.MIN_VALUE;
         // 한 자리 선택, 배치
         for (int i = 0; i < n; i++) {
             // 한 명 배치할 배열
-            copy = new char[n];
-            System.arraycopy(origin, 0, copy, 0, n);
 
             // 이미 사람이 앉아 있으면 pass
-            if (copy[i] == '1')
+            if (arr[i] == '1')
                 continue;
-            copy[i] = '1'; // 인원 배치
+            arr[i] = '1'; // 인원 배치
 
-            int dist = Integer.MAX_VALUE;
-
-            // 인원 배치 후 가장 가까운 두 사람 거리 계산
-            for (int j = 0; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (copy[j] == '1' && copy[k] == '1')
-                        dist = k - j < dist ? k - j : dist;
-                }
-            }
+            int dist = getMindist(); // 가장 가까운 두 사람 거리
             max = max < dist ? dist : max;
+
+            arr[i] = '0'; // 원상 복구
         } // end of fori
 
         System.out.println(max);
 
     }// end of main
+
+    // 가장 가까운 두사람 거리 반환
+    public static int getMindist() {
+        int min = Integer.MAX_VALUE;
+        // 둘 다 1인 곳에 대해
+        // 모든 쌍을 조사하여, 그 중 가장 가까운 거리를 구한다.
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == '1' && arr[j] == '1')
+                    min = j - i < min ? j - i : min;
+            }
+        }
+
+        return min;
+    }
 
 }// end of class
