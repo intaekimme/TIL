b96edaad38ada069da2fd897ba631601c79f29e5
InTae <kit938639@gmail.com>
2023-01-17 16:00:49 +0900

    가능한 상황을 나열하기 - 순위 경쟁2

---
+++ algo/codetree/java/list_possible_situations/Main.java
@@ -0,0 +1,53 @@
+package list_possible_situations;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 순위 경쟁 2
+ */
+public class Main {
+
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        char state = 'E'; // 명예의 전당 상태
+        int[] score = new int[2]; // A, B의 점수 상태
+        int cnt = 0; // 변경 횟수
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            char person = st.nextToken().charAt(0);
+            int s = Integer.parseInt(st.nextToken());
+
+            if (person == 'A')
+                score[0] += s;
+            else
+                score[1] += s;
+
+            char res = ' ';
+            if (score[0] > score[1]) {
+                res = 'A';
+            } else if (score[0] < score[1]) {
+                res = 'B';
+            } else {
+                res = 'E';
+            }
+
+            if (state != res) {
+                cnt++;
+                state = res;
+            }
+
+        }
+
+        System.out.println(cnt);
+
+    }// end of main
+}
