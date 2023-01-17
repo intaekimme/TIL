6b8f65a13e2c30f419762b3bb17bc974c5f9f61c
InTae <kit938639@gmail.com>
2023-01-17 14:47:25 +0900

    좋은 전략을 추려내기 - 연속된 숫자 만들기

--- algo/codetree/java/picking_out_a_good_strategy/Main_2.java
+++ algo/codetree/java/picking_out_a_good_strategy/Main_2.java
@@ -5,25 +5,28 @@ import java.util.*;
 
 /**
  * 연속된 숫자 만들기 2
- * 와....이거 진짜 모르겠다
+ * 3번의 경우면 해결됨
  */
 public class Main_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[3];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int p1 = Integer.parseInt(st.nextToken());
-        int p2 = Integer.parseInt(st.nextToken());
-        int p3 = Integer.parseInt(st.nextToken());
-
-    }// end of main
-
-    // 재귀로 찾아야 하나...?
-    public static int recur(int p1, int p2, int p3, int cnt) {
-        if (p1 < p2 && p2 < p3) {
-            return cnt;
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-    }
+        Arrays.sort(arr);
+
+        if (arr[1] - arr[0] == 1 && arr[2] - arr[1] == 1)
+            System.out.println(0);
+        else if (arr[1] - arr[0] == 2 || arr[2] - arr[1] == 2)
+            System.out.println(1);
+        else
+            System.out.println(2);
+
+    }// end of main
 
 }// end of class
