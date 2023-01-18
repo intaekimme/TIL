c164187e0f2573ec6c443f061db38c9b37353a39
InTae <kit938639@gmail.com>
2023-01-18 10:04:35 +0900

    격자 안에서 단일 객체를 이동 - 숫자가 더 큰 인접한 곳으로 이동_refactoring

--- algo/codetree/java/Intermediate_low/simulation/single/Main_1.java
+++ algo/codetree/java/Intermediate_low/simulation/single/Main_1.java
@@ -7,68 +7,82 @@ import java.util.*;
  * 숫자가 더 큰 인접한 곳으로 이동
  */
 public class Main_1 {
+    static final int DIR_NUM = 4;
+    static final int MAX_N = 100;
 
-    static int n, r, c;
-    static int[][] map;
-    static final int[] dr = { -1, 1, 0, 0 }; // 상, 하, 좌, 우
-    static final int[] dc = { 0, 0, -1, 1 };
+    static int n;
+    static int currX, currY; // 현재 위치
+    static int[][] map = new int[MAX_N + 1][MAX_N + 1];
+
+    // 범위가 격자 안에 들어가는가
+    public static boolean inRange(int x, int y) {
+        return 1 <= x && x <= n && 1 <= y && y <= n;
+    }
+
+    // 범위가 격자 안이고, 해당 위치 값이 더 큰지 확인
+    public static boolean canGo(int x, int y, int currNum) {
+        return inRange(x, y) && map[x][y] > currNum;
+    }
+
+    // 조건에 맞게 움직여 봄
+    // 조건에 맞으면 true 반환
+    // 조건에 맞지않으면 false 반환
+    public static boolean simulate() {
+        int[] dx = new int[] { -1, 1, 0, 0 };
+        int[] dy = new int[] { 0, 0, -1, 1 };
+
+        // 각각의 방향에 대해 갈 수 있는지 탐색
+        for (int i = 0; i < 4; i++) {
+            int nextX = currX + dx[i];
+            int nextY = currY + dy[i];
+
+            // 갈 수 있으면
+            // 이동하고 true 반환
+            if (canGo(nextX, nextY, map[currX][currY])) {
+                currX = nextX;
+                currY = nextY;
+                return true;
+            }
+        }
+        // 갈 수 없으므로
+        // false 반환
+        return false;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken()) - 1;
-        c = Integer.parseInt(st.nextToken()) - 1;
+        currX = Integer.parseInt(st.nextToken());
+        currY = Integer.parseInt(st.nextToken());
 
-        map = new int[n][n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= MAX_N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j <= MAX_N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        // 시작 위치 기록
         StringBuilder sb = new StringBuilder();
-        sb.append(map[r][c]).append(" "); // 초기 위치 입력
+        sb.append(map[currX][currY]).append(" ");
 
         while (true) {
-            int cnt = 0; // 이동할 수 없을 경우 카운트
-            // 4방 탐색 시작
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
+            // 조건에 맞춰 움직여봄
+            boolean greaterNumberExist = simulate();
 
-                // 나아갈 수 없는 방향이면 이동할 수 없는 경우 증가
-                if (outOfRange(nr, nc)) {
-                    cnt++;
-                    continue;
-                }
-
-                // 이동할 수 있는 경우이면 기록후 좌표값 갱신 후 탐색 중단, 우선순위 때문에
-                if (map[nr][nc] > map[r][c]) {
-                    sb.append(map[nr][nc]).append(" ");
-                    r = nr;
-                    c = nc;
-                    break;
-                }
-
-                // 탐색 중단이 안되었으면 이동할 수 없는 경우이므로 카운트 증가
-                cnt++;
-            }
-
-            // 4방 탐색 후 상하좌우 모두 이동할 수 없으면 탐색 종료
-            if (cnt == 4)
+            // 인접한 곳에 더 큰 숫자가 없다면 종료
+            if (!greaterNumberExist)
                 break;
+
+            // 움직이고 난 후 위치 기록
+            sb.append(map[currX][currY]).append(" ");
         }
 
+        // 출력
         System.out.println(sb.toString());
 
     }// end of main
-
-    public static boolean outOfRange(int r, int c) {
-        return (r < 0 || r >= n || c < 0 || c >= n);
-    }// end of outOfRange
 
 }
