ec62a9dab932a4905b8d52251204bb78697f41c4
InTae <kit938639@gmail.com>
2023-01-22 01:27:08 +0900

    :sparkles: feat 완전탐색 1 - 자리 마다 숫자를 정하는 완전탐색, 개발자의 능력2_정답코드

--- algo/codetree/java/Novice_mid/exhaustive_search_1/exhaustive_search_that_determines_the_number_for_each_digit/Main_5.java
+++ algo/codetree/java/Novice_mid/exhaustive_search_1/exhaustive_search_that_determines_the_number_for_each_digit/Main_5.java
@@ -3,21 +3,19 @@ package Novice_mid.exhaustive_search_1.exhaustive_search_that_determines_the_num
 import java.io.*;
 import java.util.*;
 
+/**
+ * 개발자의 능력 2
+ * 정답 코드
+ */
 public class Main_5 {
 
     static int[] arr;
-    static boolean[] visited;
-    static int[] team1_arr = new int[2];
-    static int[] team2_arr = new int[2];
-    static int[] team3_arr = new int[2];
-
     static int ans = Integer.MAX_VALUE;
 
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         arr = new int[6];
-        visited = new boolean[6];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 6; i++)
@@ -32,48 +30,36 @@ public class Main_5 {
         return sum;
     }
 
+    public static void updateMin(int i, int j, int k, int l) {
+        int team1 = arr[i] + arr[j];
+        int team2 = arr[k] + arr[l];
+        int team3 = sum() - (team1 + team2);
+
+        int max = Math.max(team1, Math.max(team2, team3));
+        int min = Math.min(team1, Math.min(team2, team3));
+
+        ans = Math.min(ans, max - min);
+    }// end of updateMin
+
     public static void sol() {
-        for (int i = 0; i < 6; i++) {
-            if (!visited[i]) {
-                team1_arr[0] = arr[i];
-                visited[i] = true;
-                for (int j = 0; j < 6; j++) {
-                    if (!visited[j]) {
-                        team1_arr[1] = arr[j];
-                        visited[j] = true;
-                        for (int k = 0; k < 6; k++) {
-                            if (!visited[k]) {
-                                team2_arr[0] = arr[k];
-                                visited[k] = true;
-                                for (int l = 0; l < 6; l++) {
-                                    if (!visited[l]) {
-                                        team2_arr[1] = arr[l];
+        for (int i = 0; i < 6; i++)
+            for (int j = i + 1; j < 6; j++)
 
-                                        int team1 = team1_arr[0] + team1_arr[1];
-                                        int team2 = team2_arr[0] + team2_arr[1];
-                                        int team3 = sum() - (team1 + team2);
-
-                                        int max = Math.max(team1, Math.max(team2, team3));
-                                        int min = Math.min(team1, Math.min(team2, team3));
-
-                                        ans = Math.min(ans, max - min);
-                                    }
-                                } // end of l
-                                visited[k] = false;
-                            }
-                        } // end of k
-                        visited[j] = false;
+                for (int k = 0; k < 6; k++)
+                    for (int l = k + 1; l < 6; l++) {
+                        if (k == i || k == j || l == i || l == j)
+                            continue;
+                        updateMin(i, j, k, l);
                     }
-                } // end of j
-                visited[i] = false;
-            }
-        } // end of i
 
     }// end of sol
 
     public static void main(String[] args) throws IOException {
         init();
+        // double start = System.currentTimeMillis();
         sol();
+        // double end = System.currentTimeMillis();
         System.out.println(ans);
+        // System.out.println(ans + " " + (end - start));
     }// end of main
 }
