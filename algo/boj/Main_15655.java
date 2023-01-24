4a66b53ec12de0c1abd3b75d2a1d3e2366ef3a06
InTae <kit938639@gmail.com>
2023-01-25 02:30:51 +0900

    :sparkles: feat 백트랙킹, n과 m(6)

---
+++ algo/boj/Main_15655.java
@@ -0,0 +1,55 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * n개의 서로 다른 자연수 15655
+ * 수열 오름차순
+ * 중복 x
+ * 조합
+ */
+public class Main {
+    static final int MAX_N = 8;
+    static int n, m;
+
+    static int[] arr = new int[MAX_N];
+    static int[] record = new int[MAX_N];
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void func(int depth, int start) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++)
+                sb.append(record[i]).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            record[depth] = arr[i];
+            func(depth + 1, i + 1);
+        }
+    }// end of func
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        Arrays.fill(arr, Integer.MAX_VALUE);
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        func(0, 0);
+
+        System.out.print(sb.toString());
+    }// end of main
+}
