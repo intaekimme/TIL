52793194b915411e16c36baa5f20aeb2dac386a9
InTae <kit938639@gmail.com>
2023-01-25 03:46:14 +0900

    :sparkles: feat 백트랙킹, n과 m(7)_인풋출력 추가

--- algo/boj/Main_15656.java
+++ algo/boj/Main_15656.java
@@ -4,10 +4,10 @@ import java.io.*;
 import java.util.*;
 
 /**
- * n과 m (7) 15656
+ * n과 m (7) 15656, 15651
  * 서로 다른 자연수
  * 같은 수 중복 허용
- * 
+ * 중복순열
  */
 public class Main_15656 {
 
@@ -53,3 +53,23 @@ public class Main_15656 {
         System.out.print(sb.toString());
     }// end of main
 }// end of class
+
+// 4 2
+// 9 8 7 1
+
+// 1 1
+// 1 7
+// 1 8
+// 1 9
+// 7 1
+// 7 7
+// 7 8
+// 7 9
+// 8 1
+// 8 7
+// 8 8
+// 8 9
+// 9 1
+// 9 7
+// 9 8
+// 9 9
