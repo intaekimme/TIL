84f528d8cae45d5c2981284c9099ff253c79eb51
InTae <kit938639@gmail.com>
2023-01-25 23:24:55 +0900

    :sparkles: feat 백트랙킹, n과 m(9)_풀이 2

--- algo/boj/Main_15663.java
+++ algo/boj/Main_15663.java
@@ -10,36 +10,34 @@ import java.util.*;
  * 중복되는 수열은 여러번 출력 x
  * 수열은 사전 순으로 증가하는 순서
  * 
- * 풀이 1
+ * 풀이 2
  * 순열 코드
- * 사전 순 출력을 위해 정렬 이용
- * 현재 뽑는 원소가 이전에 사용한 원소가 아니라면 뽑는다
- * 1) 방문배열로 확인
- * 2) 이전에 사용한 후보는 처음부터 확인하기 때문에 먼저 뽑은 원소와 동일한 원소를 확인할 때는 후보가 달라서 뽑힌다.
+ * 순열을 그대로 뽑는다
+ * 중복 발생
+ * set을 이용해 중복을 제거한다.
+ * 603ms
+ * 
+ * 풀이 1 : 204ms
  */
 public class Main_15663 {
 
     static int n, m;
     static int[] numbers, record;
+    static Set<String> set = new LinkedHashSet<>();
     static boolean[] used;
-
-    static StringBuilder sb = new StringBuilder();
 
     public static void func(int depth) {
         if (depth == m) {
+            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < m; i++)
                 sb.append(record[i]).append(" ");
-            sb.append("\n");
+            set.add(sb.toString());
             return;
         }
 
-        int last_cand = 0;
         for (int cand = 0; cand < n; cand++) {
             if (used[cand])
                 continue;
-            if (numbers[cand] == last_cand)
-                continue;
-            last_cand = numbers[cand];
             record[depth] = numbers[cand];
             used[cand] = true;
             func(depth + 1);
@@ -68,7 +66,7 @@ public class Main_15663 {
         used = new boolean[n];
         func(0);
 
-        System.out.print(sb.toString());
+        set.forEach(System.out::println);
     }// end of main
 
 }// end of class
