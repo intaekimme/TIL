87b4ec3c28647a9823c7ad9089f477dfbf8c956c
InTae <kit938639@gmail.com>
2023-01-26 21:17:33 +0900

    :sparkles: feat 백트랙킹, N-Queen_sol(1)

--- algo/boj/backtracking/Main_9663.java
+++ algo/boj/backtracking/Main_9663.java
@@ -27,77 +27,54 @@ public class Main_9663 {
     static int ans = 0;
 
     static boolean[][] map;
-    static int[] selected;
+    static int[] col;
 
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
 
-        map = new boolean[n][n];
-        selected = new int[n * n];
+        col = new int[n + 1];
     }// end of init
 
-    public static boolean outOfRange(int x, int y) {
-        return x < 0 || x >= n || y < 0 || y >= n;
-    }// outOfRange
+    public static boolean attackable(int r1, int c1, int r2, int c2) {
+        if (c1 == c2) // 열확인
+            return true;
+        if (r1 - c1 == r2 - c2) // 좌상단 우하단의 \ 대각선 확인
+            return true;
+        if (r1 + c1 == r2 + c2)// 우상단 좌하단의 / 대각선 확인
+            return true;
+        return false;
+    }
 
-    public static void putQueen(int x, int y) {
-        int[] dx = new int[] { -1, -1, 0, 1, 1, 1, 0, -1 };
-        int[] dy = new int[] { 0, 1, 1, 1, 0, -1, -1, -1 };
-
-        map[x][y] = true;
-
-        for (int i = 0; i < 8; i++) {
-            for (int j = 1; j <= n - 1; j++) {
-                int nx = x + dx[i] * j;
-                int ny = y + dy[i] * j;
-
-                if (outOfRange(nx, ny))
-                    continue;
-                map[nx][ny] = true;
-            }
-        }
-    }// end of putQueen
-
-    public static void fallBackQueen(int x, int y) {
-        int[] dx = new int[] { -1, -1, 0, 1, 1, 1, 0, -1 };
-        int[] dy = new int[] { 0, 1, 1, 1, 0, -1, -1, -1 };
-
-        map[x][y] = false;
-
-        for (int i = 0; i < 8; i++) {
-            for (int j = 1; j <= n - 1; j++) {
-                int nx = x + dx[i] * j;
-                int ny = y + dy[i] * j;
-
-                if (outOfRange(nx, ny))
-                    continue;
-                map[nx][ny] = false;
-            }
-        }
-    }// end of fallBackQueen
-
-    public static void func(int depth, int start) {
-        if (depth == n) {
+    public static void func(int row) {
+        if (row == n + 1) {
             ans++;
             return;
         }
 
-        for (int i = start; i < n * n; i++) {
-            int x = i / n;
-            int y = i % n;
-            if (map[x][y])
-                continue;
-            selected[depth] = i;
-            putQueen(x, y);
-            func(depth + 1, i + 1);
-            fallBackQueen(x, y);
+        // 열 하나씩 확인
+        for (int c = 1; c <= n; c++) {
+            boolean possible = true; // queen을 놓을 수 있는 여부
+            // 고른 하나의 열에서 가능한 행 확인
+            for (int i = 1; i <= row; i++) {
+                // 앞서 고른 행과 열(row, c)에 대해 새로 고른 행과 열(i, col[i])이 가능한지 판단
+                if (attackable(row, c, i, col[i])) {
+                    possible = false;
+                    break;
+                }
+            }
+            // 가능하면 row행 c열은 가능한 경우
+            if (possible) {
+                col[row] = c;
+                func(row + 1);
+                col[row] = 0;
+            }
         }
     }// end of func
 
     public static void sol() {
-        func(0, 0);
+        func(1);
     }// end of sol
 
     public static void main(String[] args) throws IOException {
