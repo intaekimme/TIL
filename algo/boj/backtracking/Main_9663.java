f0f97a82c3fc9115f58bbed65d6544ccadde01f7
InTae <kit938639@gmail.com>
2023-01-26 20:36:37 +0900

    :sparkles: feat 백트랙킹, N-Queen_out of memory

--- algo/boj/backtracking/Main_9663.java
+++ algo/boj/backtracking/Main_9663.java
@@ -1,7 +1,6 @@
 package boj.backtracking;
 
 import java.io.*;
-import java.util.*;
 
 /**
  * N-Queen, 9663
@@ -10,26 +9,33 @@ import java.util.*;
  * 
  * N이 주어졌을 때, 퀸을 노흔 방법의 수 출력
  * 
- * 풀이
+ * 풀이 1. 메모리 초과남, 시간 초과도 예상됨
  * 1. 크기 N*N인 1차원 배열에서 N개를 선택하는 조합 생각
  * 2. 조합에서 퀸을 체스판에 하나씩 놓을 때마다 공격 범위를 표시
  * 3. 조합에서 그 다음 퀸이 공격 범위에 놓이면 만족하지 않는 조합
  * 4. 조합 내 모든 퀸이 체스판 위에 놓이면 서로 공격할 수 없게 놓는 경우이므로 정답 카운트
+ * 
+ * 
+ * 풀이 2.
+ * 하나씩 놓을 수 있을 때만 놓는다
  */
 
-public class Main {
+public class Main_9663 {
     static final int MAX_N = 14;
 
     static int n;
     static int ans = 0;
 
     static boolean[][] map;
-    static int[] selected = new int[MAX_N * MAX_N];
+    static int[] selected;
 
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
+
+        map = new boolean[n][n];
+        selected = new int[n * n];
     }// end of init
 
     public static boolean outOfRange(int x, int y) {
@@ -54,30 +60,39 @@ public class Main {
         }
     }// end of putQueen
 
-    public static boolean isValid() {
-        map = new boolean[MAX_N][MAX_N];
-        for (int i = 0; i < n; i++) {
-            int x = selected[i] / n;
-            int y = selected[i] % n;
+    public static void fallBackQueen(int x, int y) {
+        int[] dx = new int[] { -1, -1, 0, 1, 1, 1, 0, -1 };
+        int[] dy = new int[] { 0, 1, 1, 1, 0, -1, -1, -1 };
 
-            if (map[x][y])
-                return false;
-            putQueen(x, y);
+        map[x][y] = false;
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 1; j <= n - 1; j++) {
+                int nx = x + dx[i] * j;
+                int ny = y + dy[i] * j;
+
+                if (outOfRange(nx, ny))
+                    continue;
+                map[nx][ny] = false;
+            }
         }
-        return true;
-    }// end of isVaild
+    }// end of fallBackQueen
 
     public static void func(int depth, int start) {
         if (depth == n) {
-            if (!isValid())
-                return;
             ans++;
             return;
         }
 
         for (int i = start; i < n * n; i++) {
+            int x = i / n;
+            int y = i % n;
+            if (map[x][y])
+                continue;
             selected[depth] = i;
+            putQueen(x, y);
             func(depth + 1, i + 1);
+            fallBackQueen(x, y);
         }
     }// end of func
 
