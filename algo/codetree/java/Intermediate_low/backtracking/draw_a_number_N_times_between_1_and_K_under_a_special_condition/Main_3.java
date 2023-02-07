0fc09fb7e6650c4dcd5f7454683073e89552786d
InTae <kit938639@gmail.com>
2023-02-08 02:56:27 +0900

    :sparkles: feat backtracking, k개 중 하나를 n번 선택하기(conditional) - 가능한 수열 중 최솟값 구하기_메모리 초과

--- algo/codetree/java/Intermediate_low/backtracking/draw_a_number_N_times_between_1_and_K_under_a_special_condition/Main_3.java
+++ algo/codetree/java/Intermediate_low/backtracking/draw_a_number_N_times_between_1_and_K_under_a_special_condition/Main_3.java
@@ -4,63 +4,67 @@ import java.io.*;
 import java.util.*;
 
 public class Main_3 {
+
+    static final int MAX_N = 80;
+    static final int[] arr = new int[] { 4, 5, 6 };
+
+    static int n;
+    static int[] selected = new int[MAX_N];
+
+    static String ans;
+    static String res;
+
+    public static String makeString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++)
+            sb.append(selected[i]);
+        return sb.toString();
+    }// end of makeString
+
+    public static boolean isValid() {
+        String arr = makeString();
+
+        for (int i = 1; i <= n / 2; i++) {
+            for (int j = 0; j <= n - 2 * i; j++) {
+                if (arr.substring(j, j + i).equals(arr.substring(j + i, j + (2 * i)))) {
+                    return false;
+                }
+            }
+        }
+        res = arr;
+        return true;
+    }// end of isValid
+
+    public static void func(int depth) {
+        if (depth == n) {
+            if (isValid()) {
+                if (res.compareTo(ans) < 0)
+                    ans = res;
+            }
+            return;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (depth >= 1 && selected[depth - 1] == arr[i])
+                continue;
+            selected[depth] = arr[i];
+            func(depth + 1);
+        }
+    }// end of func
+
     public static void main(String[] args) throws IOException {
-        int[] test = new int[] { 4, 5, 6, 4, 4 };
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String res = makeString(test);
+        n = Integer.parseInt(br.readLine());
 
-        // substring test
-        String piece1 = res.substring(0, 3);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++)
+            sb.append(6);
+        ans = sb.toString();
 
-        String piece2 = res.substring(3, res.length());
+        func(0);
 
-        System.out.println(res + " " + piece1 + " " + piece2);
-
-        // Long 최댓값 확인 : 9223372036854775807, 19자리
-        Long test2 = Long.MAX_VALUE;
-        System.out.println(test2);
-
-        String test3 = "6666666666666";
-        String test4 = "44444444";
-        String test5 = "5555555555";
-        String[] arr = new String[3];
-        arr[0] = test5;
-        arr[1] = test3;
-        arr[2] = test4;
-
-        // 문자열 sorting 전
-        // [5555555555, 6666666666666, 44444444]
-        System.out.println(Arrays.toString(arr));
-        Arrays.sort(arr);
-
-        // 문자열 sorting 후
-        // [44444444, 5555555555, 6666666666666]
-        // 사전순으로 정렬됨
-        System.out.println(Arrays.toString(arr));
-
-        // 문자열 대소비교는 compareTo
-        /**
-         * 비교 대상에 문자열이 포함되어 있는 경우
-         * 서로의 문자열 길이의 차이값을 리턴
-         * 
-         * 주의!
-         * compareTo는 같은 위치의 문자만 비교하기 때문에, 첫번째 문자부터 순서대로 비교해서 다를 경우
-         * 바로 아스키값을 기준으로 비교처리
-         * 
-         * 4와 5가 다르고
-         * 4 - 5 = -1 < 0
-         */
-
-        if (test4.compareTo(test5) < 0)
-            System.out.println("yes");
-
+        System.out.println(ans);
     }// end of main
 
-    public static String makeString(int[] arr) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            sb.append(arr[i]);
-        }
-        return sb.toString();
-    }
-}
+}// end of class
