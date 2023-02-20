c7b7489d2a18f8176e6fd9648a0bf1f1852cc238
InTae <kit938639@gmail.com>
2023-02-21 01:19:48 +0900

    :sparkles: feat Intermediate data structure, HashMap, 순서를 바꾸었을 때 같은 단어 그룹화하기_sol2

--- algo/codetree/java/Intermediate_mid/intermediate_data_structure/HashMap/Main_9.java
+++ algo/codetree/java/Intermediate_mid/intermediate_data_structure/HashMap/Main_9.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 /**
  * 순서를 바꾸었을 때 같은 단어 그룹화하기
- * 단어의 갯수를 카운트한 후 그것을 키로 만들어서 입력
+ * 단어를 char[]로 받고 그것을 정렬한 것을 키로 사용
  */
 public class Main_9 {
     public static void main(String[] args) throws IOException {
@@ -13,26 +13,17 @@ public class Main_9 {
 
         int n = Integer.parseInt(br.readLine());
 
-        int[] alph;
-
         HashMap<String, Integer> map = new HashMap<>();
 
         int max = 0;
         for (int i = 0; i < n; i++) {
-            alph = new int[58];
-            String input = br.readLine();
-            int idx = 0;
-            for (int j = 0; j < input.length(); j++) {
-                idx = input.charAt(j) - 'A';
-                alph[idx]++;
-            }
+            char[] input = br.readLine().toCharArray();
+            Arrays.sort(input);
 
             // System.out.println(Arrays.toString(alph));
 
-            String key = Arrays.toString(alph);
-            // System.out.println(map.containsKey(key) ? "Yes" : "No");
+            String key = new String(input);
             int cnt = map.getOrDefault(key, 0) + 1;
-            // System.out.println(cnt);
             max = Math.max(max, cnt);
             map.put(key, cnt);
         }
