93a04e2f937dfffdabb9b3e7cb7b8b00f5b2791c
InTae <kit938639@gmail.com>
2023-02-21 16:24:04 +0900

    :sparkles: feat fintech 교육 프로젝트 2일차 - route

--- fintech/react_project/fintech/src/App.js
+++ fintech/react_project/fintech/src/App.js
@@ -1,21 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import InputComponents from "./components/InputComponent";
 import Welcome from "./components/Welcome";
-import InputComponent from "./components/InputComponent";
-import ListComponents from "./components/ListComponents";
-import StyledComponent from "./components/StyledComponent";
 
 function App() {
   return (
-    <div className="App">
-      <Welcome userName="김인태" userAge={29} userHeight={175}></Welcome>
-      <Welcome userName="홍길동" userAge={48} userHeight={172}></Welcome>
-      <Welcome userName="서장훈" userAge={50} userHeight={190}></Welcome>
-      <Welcome userName="요정" userAge={1000} userHeight={1}></Welcome>
-      <InputComponent></InputComponent>
-      <ListComponents></ListComponents>
-      <StyledComponent></StyledComponent>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />}></Route>
+        <Route path="/input" element={<InputComponents />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
