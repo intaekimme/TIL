8a03139dc224a7782a2cb155daf88e62e59c736e
InTae <kit938639@gmail.com>
2023-02-21 13:08:39 +0900

    :sparkles: feat fintech 교육 프로젝트 2일차 - js 객체_2

---
+++ fintech/react_project/prac/example04.js
@@ -0,0 +1,8 @@
+let initArray = [];
+let carArray = ["sonata", "bmw", "tico", 123123];
+
+//  java
+//  private String[] carArray = new String[5];
+//  private ArrayList<String> = new ArrayList<String>;
+
+console.log(carArray);
---
+++ fintech/react_project/prac/example05.js
@@ -0,0 +1,12 @@
+let car = {
+  name: "sonata",
+  ph: 160,
+  year: 2022,
+};
+
+const { name, ph, year } = car;
+//  let name = car.name;
+//  let ph = car.ph;
+//  let year = car.year;
+
+console.log(name, ph, year);
