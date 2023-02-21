a94438320d3a61bf1862704bb15ba403fe175d56
InTae <kit938639@gmail.com>
2023-02-21 17:03:06 +0900

    :sparkles: feat fintech 교육 프로젝트 2일차 - axios

--- fintech/react_project/fintech/src/App.js
+++ fintech/react_project/fintech/src/App.js
@@ -5,6 +5,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InputComponents from "./components/InputComponent";
 import Welcome from "./components/Welcome";
+import ListComponents from "./components/ListComponents";
+import AxiosComponents from "./components/AxiosComponent";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />}></Route>
         <Route path="/input" element={<InputComponents />}></Route>
+        <Route path="/list" element={<ListComponents />}></Route>
+        <Route path="/axios" element={<AxiosComponents />}></Route>
       </Routes>
     </BrowserRouter>
   );
