278302428ba587c26885982745e14b2ff7b339df
InTae <kit938639@gmail.com>
2023-02-21 16:22:20 +0900

    :sparkles: feat fintech 교육 프로젝트 2일차 - styled-components까지

--- fintech/react_project/fintech/src/App.js
+++ fintech/react_project/fintech/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Welcome from "./components/Welcome";
+import InputComponent from "./components/InputComponent";
+import ListComponents from "./components/ListComponents";
+import StyledComponent from "./components/StyledComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome userName="김인태" userAge={29} userHeight={175}></Welcome>
+      <Welcome userName="홍길동" userAge={48} userHeight={172}></Welcome>
+      <Welcome userName="서장훈" userAge={50} userHeight={190}></Welcome>
+      <Welcome userName="요정" userAge={1000} userHeight={1}></Welcome>
+      <InputComponent></InputComponent>
+      <ListComponents></ListComponents>
+      <StyledComponent></StyledComponent>
     </div>
   );
 }
