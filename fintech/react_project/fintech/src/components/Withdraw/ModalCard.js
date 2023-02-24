f864c355320f8affb3f1f48037b29b7d343689f1
InTae <kit938639@gmail.com>
2023-02-24 11:06:49 +0900

    :sparkles: feat fintech 교육 프로젝트 5일차 - 출금 이체 기능 구현

--- fintech/react_project/fintech/src/components/Withdraw/ModalCard.js
+++ fintech/react_project/fintech/src/components/Withdraw/ModalCard.js
@@ -29,20 +29,61 @@ const WithDrawButton = styled.button`
 const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
   //fintechUseNo : 내계좌
   //tofintechno : QR 코드로 읽어온 핀테크 계좌
+  const accessToken = localStorage.getItem("accessToken");
+
   const [amount, setamount] = useState("");
 
   const genTransId = () => {
     let countnum = Math.floor(Math.random() * 1000000000) + 1;
-    let transId = "M202201564U" + countnum; //이용기과번호 본인것 입력
+    const clientNo = "M202300409";
+    let transId = clientNo + "U" + countnum; //이용기관번호 본인것 입력
     return transId;
   };
 
-  const handlePayButtonClick = () => {};
+  const handlePayButtonClick = () => {
+    //  출금 이체 발생시키기
+    //  data params json
+    //  tran_amg, fintech_use_num, requ_client_fintech_use
+    //  axios option으로 요청을 작성하기 <-  api 요청
+    //  application/jspn 은 데이터를 어떻게 전송?
+    //  결과를 로그로 작성
+    const data = {
+      bank_tran_id: genTransId(),
+      cntr_account_type: "N",
+      cntr_account_num: "100000000001",
+      dps_print_content: "사용료(김인태)",
+      fintech_use_num: fintechUseNo,
+      wd_print_content: "사용료",
+      tran_amt: amount,
+      tran_dtime: "20230224103900",
+      req_client_name: "김인태",
+      req_client_fintech_use_num: fintechUseNo,
+      req_client_num: "KIT938639",
+      transfer_purpose: "ST",
+      recv_client_name: "김인태",
+      recv_client_bank_code: "097",
+      recv_client_account_num: "100000000001",
+    };
+
+    const option = {
+      method: "POST",
+      url: "/v2.0/transfer/withdraw/fin_num",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: data,
+    };
+
+    axios(option).then(({ data }) => {
+      console.log(data);
+    });
+  };
 
   const deposit = () => {};
 
   const handleChange = (e) => {
     const { value } = e.target;
+    console.log(value);
     setamount(value);
   };
 
