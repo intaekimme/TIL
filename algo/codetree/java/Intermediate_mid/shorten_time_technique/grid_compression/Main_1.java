1bdc62a62a8dab3e33bd0e83f221c952575889ae
InTae <kit938639@gmail.com>
2023-02-28 14:29:48 +0900

    :sparkles: feat Intermediate_mid, shorten_time_technique, grid_compression, 점 개수 세기 3_fail

---
+++ algo/codetree/java/Intermediate_mid/shorten_time_technique/grid_compression/Main_1.java
@@ -0,0 +1,48 @@
+package Intermediate_mid.shorten_time_technique.grid_compression;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 점 개수 세기 3_fail
+ */
+public class Main_1 {
+
+    static int n, q;
+    static HashMap<Integer, Integer> mapper = new HashMap<>();
+    static int[] input;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+
+        input = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            input[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(input);
+
+        for (int i = 0; i < n; i++)
+            mapper.put(input[i], i + 1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < q; i++) {
+            int cnt = 0;
+            st = new StringTokenizer(br.readLine());
+
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            for (int j = s; j <= e; j++)
+                if (mapper.containsKey(j))
+                    cnt++;
+            sb.append(cnt).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }// end of main
+
+}// end of class
