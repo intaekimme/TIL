7d05e9ea00927195b7cd9b074bbec8a544d59a1a
InTae <kit938639@gmail.com>
2023-03-02 17:08:24 +0900

    :sparkles: feat boj_14503_로봇청소기_g5, simulation, sol

--- algo/boj/simulation/Main_14503.java
+++ algo/boj/simulation/Main_14503.java
@@ -1,10 +1,10 @@
-package boj.bfs;
+package boj.simulation;
 
 import java.io.*;
 import java.util.*;
 
 /**
- * 14503 로봇청소기_fail
+ * 14503 로봇청소기_sol
  * simulation
  */
 
@@ -13,9 +13,8 @@ public class Main_14503 {
     static int n, m, sr, sc, dir;
     static int ans = 0;
     static int[][] map; // 0 : 청소 안한 칸, 1: 벽, 2 : 청소한 칸
-    static boolean[][] visited;
 
-    static int[] dx = new int[] { -1, 0, 1, 0 };
+    static int[] dx = new int[] { -1, 0, 1, 0 }; // 북, 동, 남, 서
     static int[] dy = new int[] { 0, 1, 0, -1 };
 
     public static void init() throws IOException {
@@ -38,43 +37,62 @@ public class Main_14503 {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        visited = new boolean[n][m];
     }// end of init
 
-    public static boolean outOfRange(int x, int y) {
-        return x < 0 || x >= n || y < 0 || y >= m;
-    }// end of outOfRange
-
-    public static boolean canGo(int x, int y) {
-        if (outOfRange(x, y))
-            return false;
-        if (visited[x][y] || map[x][y] == 1 || map[x][y] == 2)
-            return false;
-        return true;
-    }// end of canGo
-
-    public static void dfs(int x, int y) {
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-
-            if (!canGo(nx, ny))
-                continue;
-
-            visited[nx][ny] = true;
-            map[nx][ny] = 2;
-            dfs(nx, ny);
-        }
-
-    }// end of dfs
-
     public static void sol() {
-        visited[sr][sc] = true;
-        map[sr][sc] = 2;
-        ans++;
+        int x = sr;
+        int y = sc;
 
-        dfs(sr, sc);
+        while (true) {
+
+            // 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
+            if (map[x][y] == 0) {
+                map[x][y] = 2;
+                ans++;
+            }
+
+            // 현재 칸의 주변 4칸을 확인한다.
+            int cnt = 0;
+            for (int i = 0; i < 4; i++) {
+                dir = (dir + 3) % 4;
+
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                // 청소되지 않은 빈 칸인 경우
+                if (map[nx][ny] == 0) {
+                    // 해당 칸으로 한 칸 전진한다.
+                    x = nx;
+                    y = ny;
+                    break;
+                } else {
+                    cnt++;
+                }
+
+            }
+
+            // 주변 4칸중 청소되지 않은 빈 칸이 없는 경우
+            if (cnt == 4) {
+                int nx = x + dx[(dir + 2) % 4];
+                int ny = y + dy[(dir + 2) % 4];
+
+                // 후진한 칸이 벽이라면 작동을 멈춘다.
+                if (map[nx][ny] == 1)
+                    break;
+                else {
+                    // 후진할 수 있다면 후진한다.
+                    x = nx;
+                    y = ny;
+                }
+            }
+
+            // for (int i = 0; i < n; i++)
+            // System.out.println(Arrays.toString(map[i]));
+            // System.out.println("-----------------------------");
+        } // end of while
+
+        System.out.println(ans);
+
     }// end of sol
 
     public static void main(String[] args) throws IOException {
