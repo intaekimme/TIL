d3897f394a0df4324019b0b9d7639efa6febef3e
InTae <kit938639@gmail.com>
2023-03-27 22:40:53 +0900

    :sparkles: feat boj_1235_학생 번호_s4, sol

--- algo/boj/Main_1235.java
+++ algo/boj/Main_1235.java
@@ -4,7 +4,8 @@ import java.io.*;
 import java.util.*;
 
 /**
- * 1235 학생 번호 s4 fail
+ * 1235 학생 번호 s4 sol
+ * 인덱스를 0번까지 이동시키지 않아 틀렸었음
  */
 
 public class Main_1235 {
@@ -21,25 +22,27 @@ public class Main_1235 {
             arr[i] = br.readLine();
         }
 
-        int ans = 0;
+        int ans = 1;
 
         int len = arr[0].length();
-        for (int k = len - 1; k > 0; k--) {
+        for (int k = len - 1; k >= 0; k--) {
             HashSet<String> set = new HashSet<>();
 
             boolean flag = true;
             for (int i = 0; i < n; i++) {
                 String val = arr[i].substring(k);
+                // System.out.print(val + " ");
                 if (set.contains(val)) {
                     flag = false;
                     break;
                 }
                 set.add(val);
             }
+            // System.out.println();
             if (flag) {
-                ans = len - k;
                 break;
             }
+            ans++;
         } // end of for
 
         System.out.println(ans);
