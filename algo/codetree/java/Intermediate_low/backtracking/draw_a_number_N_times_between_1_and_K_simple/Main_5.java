4be646c739807847cfafa2e6f8b12008ea9d0568
InTae <kit938639@gmail.com>
2023-03-28 22:47:02 +0900

    :sparkles: feat Intermediate_low, backtracking, draw_a_number_N_times_between_1_and_K_simple, 사다리 타기 설계

---
+++ algo/codetree/java/Intermediate_low/backtracking/draw_a_number_N_times_between_1_and_K_simple/Main_5.java
@@ -0,0 +1,27 @@
+package Intermediate_low.backtracking.draw_a_number_N_times_between_1_and_K_simple;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 사다리 타기
+ * 할 일
+ * 
+ * 1. 사다리를 구현하고 이를 시뮬레이션 해서 어떤 숫자가 배열되는지 확인하기
+ * 1 - 1. 사다리 구현 시 유의 사항
+ * 가로줄의 길이는 최소 2 이상
+ * 가로줄과 가로줄 사이 띄어진 정도도 2 이상
+ * 1 - 2. 가로줄 최소 높이와 최대 높이 기록하기 -> 최대 높이 - 최소 높이 + 1 이 가능한 가로줄 갯수 * 세로줄 수 - 1 =
+ * 생길 수 있는 가로줄의 갯수 K
+ * 
+ * 2. 숫자가 나온다면
+ * 2 - 1. 가로줄 1개부터 가로 줄 K개의 가로줄까지 뽑아보면서 시뮬레이션
+ * 이 때, 주의사항
+ * 두 개의 가로줄이 겹쳐서 등장할 수 없음, 이걸 감안하면서 가로줄 뽑아야함.
+ */
+
+public class Main_5 {
+    public static void main(String[] args) throws IOException {
+
+    }// end of main
+}// end of class
