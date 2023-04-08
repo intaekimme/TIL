7d515466727f840a80acde31ddeccf3e8186c351
InTae <kit938639@gmail.com>
2023-04-08 17:17:28 +0900

    :sparkles: feat boj_19236_청소년 상어_g2, sol

--- algo/boj/simulation/Main_19236.java
+++ algo/boj/simulation/Main_19236.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 /**
- * 청소년 상어 19236 g2 fail
+ * 청소년 상어 19236 g2
  * 
  * 물고기 -> 상어 순으로 이동
  * 
@@ -32,7 +32,7 @@ public class Main_19236 {
     static int[] dx = new int[] { -1, -1, 0, 1, 1, 1, 0, -1 };
     static int[] dy = new int[] { 0, -1, -1, -1, 0, 1, 1, 1 };
 
-    static ArrayList<Fish> fishes = new ArrayList<>();
+    static Fish[] fishes = new Fish[16];
 
     // 물고기와 상어가 있는 맵;
     // 1 ~ 16 : 물고기
@@ -40,7 +40,7 @@ public class Main_19236 {
     // -1 : 상어
     static int[][] map;
 
-    static Shark shark;
+    static int ans = 0; // 최종 정답
 
     // 초기화
     public static void init() throws IOException {
@@ -50,22 +50,17 @@ public class Main_19236 {
 
         map = new int[4][4];
 
-        fishes.add(new Fish(-1, -1, 5, 5));
-
         for (int i = 0; i < 4; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 4; j++) {
-                int id = Integer.parseInt(st.nextToken());
+                int id = Integer.parseInt(st.nextToken()) - 1;
                 int dir = Integer.parseInt(st.nextToken()) - 1;
 
-                Fish fish = new Fish(id, dir, i, j);
-                fishes.add(fish);
+                fishes[id] = new Fish(dir, i, j, false);
 
                 map[i][j] = id;
             }
         }
-
-        Collections.sort(fishes);
 
     }// end of init
 
@@ -73,115 +68,93 @@ public class Main_19236 {
         return x < 0 || x >= 4 || y < 0 || y >= 4;
     }// end of outOfRange
 
-    // 상어가 이동할 수 있는 칸이 있는지 없는지 판단
-    // 공간 내에서 상어가 바라보고 있는 방향으로 물고기가 한 마리라도 존재하면 이동할 수 있다.
-    // 그렇지 않으면 이동할 수 없다.
-    public static boolean canNotGo(Shark shark) {
-        int cur_x = shark.x;
-        int cur_y = shark.y;
+    // 재귀 함수
+    // 상어의 이동과 물고기의 이동이 반복되는 함수
+    // 상어 x, y, 먹은 먹이
+    public static void dfs(int[][] origin_map, Fish[] origin_fishes, int shark_x, int shark_y, int sum) {
+        // 백트랙킹을 위한 백업
+        // 어느 지점이 최적인지 모름, 그 동안 상태가 변경됨, 아니면 이전 상태로 돌려야함 그래서 로컬로 백업함
+        int[][] map = new int[4][4];
+        for (int i = 0; i < 4; i++)
+            System.arraycopy(origin_map[i], 0, map[i], 0, 4);
 
-        int dir = shark.dir;
-
-        int fish_cnt = 0; // 상어가 바라보고 있는 방향에서 물고기 수
-        for (int i = 0; i < 3; i++) {
-            cur_x += dx[dir];
-            cur_y += dy[dir];
-
-            if (outOfRange(cur_x, cur_y))
-                break;
-
-            // 바라보는 방향에 물고기가 존재하면
-            if (map[cur_x][cur_y] > 0)
-                fish_cnt++;
+        Fish[] fishes = new Fish[16];
+        for (int i = 0; i < 16; i++) {
+            Fish f = origin_fishes[i];
+            fishes[i] = new Fish(f.dir, f.x, f.y, f.isDead);
         }
 
-        // 바라보고 있는 방향에 물고기가 존재하지 않으면 이동할 수 없어 집으로 이동
-        if (fish_cnt == 0)
-            return true;
-        return false; // 이동할 수 있음
+        // 상어가 물고기를 먹는다.
+        int fish_number = map[shark_x][shark_y];
+        int shark_dir = fishes[fish_number].dir;
 
-    }// end of canNotGo
+        // 죽은 물고기는 -1 처리
+        fishes[fish_number].x = -1;
+        fishes[fish_number].y = -1;
+        fishes[fish_number].dir = -1;
+        fishes[fish_number].isDead = true;
+        map[shark_x][shark_y] = -1;
 
-    public static void findAndMove(Fish fish) {
-        int dir = fish.dir;
+        sum += fish_number + 1;
+        ans = Math.max(ans, sum);
 
-        int x = fish.x;
-        int y = fish.y;
+        // 물고기가 이동한다.
+        // 모든 물고기에 대해
+        for (int i = 0; i < 16; i++) {
+            if (fishes[i].isDead)
+                continue;
 
-        for (int i = 0; i < 8; i++) {
-            int nx = x + dx[(dir + i) % 8];
-            int ny = y + dy[(dir + i) % 8];
+            int cx = fishes[i].x;
+            int cy = fishes[i].y;
+            int cd = fishes[i].dir;
+
+            int nx = cx + dx[cd];
+            int ny = cy + dy[cd];
+            int nd = cd;
+
+            // 범위 밖이거나 상어가 있으면
+            while (outOfRange(nx, ny) || (nx == shark_x && ny == shark_y)) {
+                nd = (nd + 1) % 8;
+                nx = cx + dx[nd];
+                ny = cy + dy[nd];
+            }
+
+            if (map[nx][ny] != -1) {
+                int target = map[nx][ny];
+                fishes[target].x = cx;
+                fishes[target].y = cy;
+                fishes[i].x = nx;
+                fishes[i].y = ny;
+                fishes[i].dir = nd;
+
+                map[nx][ny] = i;
+                map[cx][cy] = target;
+            } else {
+                fishes[i].x = nx;
+                fishes[i].y = ny;
+                fishes[i].dir = nd;
+
+                map[nx][ny] = i;
+                map[cx][cy] = -1;
+            }
+        }
+
+        // 상어가 이동한다. -> 상어가 물고기를 먹는다.
+        for (int step = 1; step < 4; step++) {
+            int nx = shark_x + dx[shark_dir] * step;
+            int ny = shark_y + dy[shark_dir] * step;
 
             if (outOfRange(nx, ny))
-                continue;
-            // 빈 칸이거나 물고기가 있는 칸이면
-            // 빈 칸이면
-            if (map[nx][ny] == 0) {
-                fish.dir = (dir + i) % 8;
-                fish.x = nx;
-                fish.y = ny;
-
-                map[x][y] = 0;
-                map[nx][ny] = fish.id;
                 break;
-            }
-            if (map[nx][ny] > 0) {
-                fish.dir = (dir + i) % 8;
-                int another_id = map[nx][ny];
-                Fish another = fishes.get(another_id);
-                Fish tmp = another;
-
-                map[x][y] = another_id;
-                map[nx][ny] = fish.id;
-
-                another = fish;
-                fish = tmp;
-                break;
-            }
-        }
-    }// end of findAndMove
-
-    // 재귀 함수
-    public static void dfs(Shark shark) {
-
-        // 기저 조건
-        if (canNotGo(shark)) {
-            return;
-        }
-
-        // 물고기 이동
-        // 번호가 작은 물고기 중 살아있는 물고기만 이동
-        for (int i = 1; i <= fishes.size(); i++) {
-            Fish fish = fishes.get(i);
-            // 죽은 물고기는 이동에서 제외
-            if (!fish.alive)
-                continue;
-
-            System.out.println(fishes.toString());
-            findAndMove(fish);
-            System.out.println(fishes.toString());
+            if (map[nx][ny] != -1)
+                dfs(map, fishes, nx, ny, sum);
         }
 
     }// end of dfs
 
     public static void simulation() {
-        // (0, 0)에 상어 투입
-        int id = map[0][0];
-
-        Fish f = fishes.get(id);
-
-        shark = new Shark(f.dir, 0, 0);
-        f.alive = false;
-        map[0][0] = -1;
-
-        // System.out.println(fishes.toString());
-
-        // fishes.remove(id);
-        // fishes.add(id, null);
-        // System.out.println(fishes.toString());
-
-        dfs(shark);
-
+        dfs(map, fishes, 0, 0, 0);
+        System.out.println(ans);
     }// end of simulation
 
     public static void main(String[] args) throws IOException {
@@ -191,43 +164,16 @@ public class Main_19236 {
 
 }// end of class
 
-class Shark {
+class Fish {
     int dir;
     int x;
     int y;
+    boolean isDead;
 
-    public Shark(int dir, int x, int y) {
+    public Fish(int dir, int x, int y, boolean isDead) {
         this.dir = dir;
         this.x = x;
         this.y = y;
-    }
-}// end of Shark
-
-class Fish implements Comparable<Fish> {
-    int id;
-    int dir;
-    int x;
-    int y;
-    boolean alive;
-
-    public Fish(int id, int dir, int x, int y) {
-        this.id = id;
-        this.dir = dir;
-        this.x = x;
-        this.y = y;
-        this.alive = true;
-    }
-
-    @Override
-    public int compareTo(Fish f) {
-        return this.id - f.id; // 오름차순 정렬
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(this.id).append(" ").append(this.dir);
-
-        return sb.toString();
+        this.isDead = isDead;
     }
 }// end of class
