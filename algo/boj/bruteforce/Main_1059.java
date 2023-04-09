e67ffa5106a1919be42c17f258e3e919dc1955d4
InTae <kit938639@gmail.com>
2023-04-10 00:35:19 +0900

    :sparkles: feat boj_1059_좋은 구간_s4_sol

--- algo/boj/bruteforce/Main_1059.java
+++ algo/boj/bruteforce/Main_1059.java
@@ -14,8 +14,12 @@ public class Main_1059 {
 
     public static void dfs(int depth, int s, int e, int[] selected, int n) {
         if (depth == 2) {
-            for (int i = selected[0]; i <= selected[1]; i++) {
-                if (i == n) {
+            // A <= x <= B
+            int A = selected[0];
+            int B = selected[1];
+            for (int x = A; x <= B; x++) {
+                // A <= x <= B 이면 조건을 만족하는 좋은 구간
+                if (x == n) {
                     // System.out.println(selected[0] + " " + selected[1]);
                     cnt++;
                     return;
@@ -24,6 +28,8 @@ public class Main_1059 {
             return;
         }
 
+        // 가능한 구간 선정
+        // s(S[i])보다 큰 값과 e(S[i + 1]) 보다 작은값, 왜냐하면 x 가 집합 S에 속하지 않으므로
         for (int cand = s + 1; cand < e; cand++) {
             selected[depth] = cand;
             dfs(depth + 1, cand, e, selected, n);
@@ -37,20 +43,31 @@ public class Main_1059 {
         int[] arr = new int[L];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 집합을 입력 받는다.
         for (int i = 0; i < L; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         int n = Integer.parseInt(br.readLine());
 
+        // 집합 정렬
         Arrays.sort(arr);
 
         int[] tmp = new int[L + 1];
+        // 집합의 최솟값이 1이 아니라면, 최솟값보다 작은값을 구간의 시작으로 잡고 확인할 때 정수 x가 포함될 수 있음
+        // 아래 예시 참조
+        // 그러므로 1이 아니면 최솟값을 0을 추가
         if (arr[0] != 1) {
-            for (int i = 1; i < L + 1; i++)
+            // 임시 배열에 복사
+            for (int i = 1; i < tmp.length; i++)
                 tmp[i] = arr[i - 1];
+
+            // 원본 배열 크기 재설정
             arr = new int[L + 1];
-            for (int i = 0; i < L + 1; i++)
+
+            // 원복
+            for (int i = 0; i < arr.length; i++)
                 arr[i] = tmp[i];
         }
 
@@ -59,6 +76,7 @@ public class Main_1059 {
             int s = arr[i];
             int e = arr[i + 1];
 
+            // 구간의 시작을 s, e로 하는 경우에 가능한 모든 구간 찾기
             dfs(0, s, e, selected, n);
         }
 
@@ -66,6 +84,9 @@ public class Main_1059 {
     }// end of main
 }// end of class
 
+// 예시
 // 3
 // 7 8 9
 // 2
+// ans : 9
+// [1 2], [1 3], [1 4], [1 5], [1 6], [2 3], [2 4], [2 5], [2 6]
