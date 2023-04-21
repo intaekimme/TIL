c0c171128366f49366b67d1d923eb9b1d6e176b8
InTae <kit938639@gmail.com>
2023-04-21 23:55:16 +0900

    :sparkles: feat boj_17144_미세먼지안녕_g4_푸는중

---
+++ algo/boj/simulation/Main_17144.java
@@ -0,0 +1,58 @@
+package boj.simulation;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 미세먼지 안녕
+ * 17144, g4
+ * 
+ */
+
+public class Main {
+
+    static int R, C, T;
+    static int[][] map;
+
+    static int[] up = new int[2];
+    static int[] down = new int[2];
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+
+        map = new int[R][C];
+
+        int val_cnt = 0;
+        for (int i = 0; i < R; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < C; j++) {
+                int val = Integer.parseInt(st.nextToken());
+                if (val == -1) {
+                    if (val_cnt == 0)
+                        up = new int[] { i, j };
+                    else
+                        down = new int[] { i, j };
+                }
+                map[i][j] = val;
+            }
+        }
+    }// end of init
+
+    public static void simulation() {
+        while (T-- > 0) {
+
+        }
+    }// end of simulation
+
+    public static void main(String[] args) throws IOException {
+        init();
+        simulation();
+
+    }// end of main
+}// end of class
