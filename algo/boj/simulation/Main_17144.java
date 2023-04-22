518977e9fa827681bbe752e187d632032801861a
InTae <kit938639@gmail.com>
2023-04-22 20:27:46 +0900

    :sparkles: feat boj_17144_미세먼지안녕_g4_푸는중2

--- algo/boj/simulation/Main_17144.java
+++ algo/boj/simulation/Main_17144.java
@@ -17,6 +17,9 @@ public class Main_17144 {
     static int[] up = new int[2];
     static int[] down = new int[2];
 
+    static int[] dx = new int[4];
+    static int[] dy = new int[4];
+
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -44,9 +47,90 @@ public class Main_17144 {
         }
     }// end of init
 
+    public static boolean outOfRange(int x, int y) {
+        return x < 0 || x >= R || y < 0 || y >= C;
+    }
+
+    public static void diffusion() {
+        int[][] plus = new int[R][C];
+        int[][] minus = new int[R][C];
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                int x = i;
+                int y = j;
+
+                if (map[x][y] <= 0)
+                    continue;
+
+                int cnt = 0;
+                for (int k = 0; k < 4; k++) {
+                    int nx = x + dx[k];
+                    int ny = y + dy[k];
+
+                    if (outOfRange(nx, ny) || map[nx][ny] == -1)
+                        continue;
+                    plus[nx][ny] += map[x][y] / 5;
+                    cnt++;
+                }
+                minus[x][y] = cnt;
+            }
+        }
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                map[i][j] = map[i][j] - (map[i][j] / 5) * minus[i][j] + plus[i][j];
+            }
+        }
+
+    }// end of diffusion
+
+    public static void rotate(int x, int y, boolean isCW) {
+        if (isCW) {
+            //  시계방향
+            int[] tmp = new int[4];
+            tmp[0] = map[x][y];
+            tmp[1] = map[x][C];
+            tmp[2] = map[0][C];
+            tmp[3] = map[0][0];
+
+
+
+
+        } else {
+            //  반시계 방향
+            int[] tmp = new int[4];
+            tmp[0] = map[x][y];
+            tmp[1] = map[x][C];
+            tmp[2] = map[R][C];
+            tmp[3] = map[R][y];
+
+            int[] copy = new int[C];
+            //  우
+            for (int i = C - 1; i >= 0; i--) {
+                map[x][i] = map[x][i - 1];
+            }
+            
+            //  하
+            for (int i = R - 1; i >= x; i--) {
+                map[i][C] = map[i - 1][C];
+            }
+            map[x + 1][C] = tmp[1];
+            
+            //  좌
+            for (int i = 0; i < C; i++) {
+                map[R - 1][i] = map[R - 1][]
+            }
+            //  상
+
+        }
+    }// end of rotate
+
     public static void simulation() {
         while (T-- > 0) {
-
+            diffusion();
+            rotate(up[0], up[1], true);
+            rotate(down[0], down[1], false);
         }
     }// end of simulation
 
