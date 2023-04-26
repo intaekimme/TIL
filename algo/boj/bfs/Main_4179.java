2284eff968f412242c20c64fe294c9987a81f9c1
InTae <kit938639@gmail.com>
2023-04-26 23:59:17 +0900

    :sparkles: feat boj_4179_불_푸는중

--- algo/boj/bfs/Main_4179.java
+++ algo/boj/bfs/Main_4179.java
@@ -1,12 +1,35 @@
 package boj.bfs;
 
+import java.io.*;
+import java.util.*;
+
 /**
  * 불 4179
  * g4
  */
 
 public class Main_4179 {
-    public static void main(String[] args) throws IOException {
 
+    static int R, C;
+    static char[][] map;
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        map = new char[R][C];
+
+        for (int i = 0; i < R; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
+
+    }// end of init
+
+    public static void main(String[] args) throws IOException {
+        init();
     }// end of main
 }// end of class
