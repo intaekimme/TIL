b16adf9fa5f93b81cd2a36278adef67db38d1a51
InTae <kit938639@gmail.com>
2024-02-06 23:40:43 +0900

    :sparkles: feat programmers algorithm_practice_kit hash Solution_1 answer

---
+++ algo/programmers/algorithm_practice_kit/hash/Solution_1.java
@@ -0,0 +1,17 @@
+package programmers.algorithm_practice_kit.hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution_1 {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++)
+            set.add(nums[i]);
+
+        if (set.size() >= nums.length / 2)
+            return nums.length / 2;
+        return set.size();
+    }
+}
